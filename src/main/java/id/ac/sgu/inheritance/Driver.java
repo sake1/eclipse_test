7582baf975e423e4b7f37156880c92f8cf216cae
sake1 <timothydickys@gmail.com>
2017-11-09 22:14:53 +0700

    inheritance VER2

--- src/main/java/id/ac/sgu/inheritance/Driver.java
+++ src/main/java/id/ac/sgu/inheritance/Driver.java
@@ -2,10 +2,14 @@ package id.ac.sgu.inheritance;
 
 public class Driver {
 
+	private static final int CAR_ACCELERATION        = 4;
+	private static final int MOTORCYCLE_ACCELERATION = 5;
+	private static final int BICYCLE_ACCELERATION    = 2;
+	
 	public static void main(String[] args) {
-		Car car = new Car("Avanza", "Toyota", 4, 1000);
-		Motorcycle motor = new Motorcycle("Blade", "Honda", 5, "Black");
-		Bicycle bicycle = new Bicycle("Carbon Fiber Bicycle", "SAVA", 2, "MTB Mountain Bike");
+		Car car = new Car("Avanza", "Toyota", CAR_ACCELERATION, 1000);
+		Motorcycle motor = new Motorcycle("Blade", "Honda", MOTORCYCLE_ACCELERATION, "Black");
+		Bicycle bicycle = new Bicycle("Carbon Fiber Bicycle", "SAVA", BICYCLE_ACCELERATION, "MTB Mountain Bike");
 		
 		car.drive(75);
 		motor.drive(55);
